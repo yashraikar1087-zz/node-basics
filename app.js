753f34b01a748ac673d198eaf0670af0ff2cfc77
Test <test@Yash.local>
2019-03-25 21:40:29 -0600

    using chalk

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const validator = require("validator");
+const chalk = require("chalk");
 
 const notes = require("./notes");
 
 console.log(validator.isEmail("yash@gmail.com"));
+
+console.log(chalk.green("I am a green line "));
